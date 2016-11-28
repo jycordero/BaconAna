7b831261c094d460e95f8aaf0c749b5943e2058d
naodell <Nathaniel.Jay.Odell@cern.ch>
2016-11-27 20:49:13 -0600

    Modifying TJet.hh to include bmva

--- DataFormats/interface/TJet.hh
+++ DataFormats/interface/TJet.hh
@@ -12,7 +12,7 @@ namespace baconhep
       TJet():
       pt(0), eta(0), phi(0), mass(0), ptRaw(0), unc(0),
       area(0), d0(-999),dz(-999),
-      csv(-2), qgid(-2), q(0),
+      csv(-2), bmva(-2), qgid(-2), q(0),
       mva(-2), beta(0), betaStar(0), dR2Mean(0), ptD(0), minW(0),
       pullY(0), pullPhi(0), chPullY(0), chPullPhi(0), neuPullY(0), neuPullPhi(0),
       chEmFrac(0), neuEmFrac(0), chHadFrac(0), neuHadFrac(0),
@@ -26,7 +26,7 @@ namespace baconhep
       float          pt, eta, phi, mass, ptRaw, unc;              // kinematics
       float          area;                                        // jet area (from FastJet)
       float          d0, dz;                                      // impact parameter of leading charged constituent
-      float          csv;                                         // CSV b-tagger
+      float          csv, bmva;                                   // CSV b-tagger, MVA b tagger
       float          qgid;                                        // q/g discriminator
       float          q;                                           // jet charge
       float          mva;                                         // Pile-up discriminator MVA
